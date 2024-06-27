b3e3fc5ad13fcd2553fa5cb6bb36f90f1e88e850
Muhammad Bisyri Mustofa <mbisyri33@gmail.com>
2024-06-27 12:51:23 +0700

    SQL % python constraints

--- server/addons/estate/models/estate_property.py
+++ server/addons/estate/models/estate_property.py
@@ -1,8 +1,32 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
+
+    # sql constraints for validation error.
+    _sql_constraints = [
+        (
+            'unique_name',
+            'UNIQUE(name)',
+            'The name must be unique.'
+        ),
+        (
+            'check_percentage',
+            'CHECK(facades >= 0 AND facades <= 10)',
+            'The number input should be between 0 and 10.'
+        ),      
+    ]
+
+    # python constraints for validation error.
+    @api.constrains('selling_price', 'expected_price')
+    def _checking_price(self):
+        for record in self:
+            if record.selling_price <= 0:
+                raise  ValidationError("The selling price must be positive value.")
+            if record.expected_price <= record.selling_price:
+                raise ValidationError("The expected value must higher than selling price")
 
 
     name = fields.Char(string="name", required=True)
