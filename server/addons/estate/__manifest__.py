5f58aa3856e5ca4d4725122eaebe1895ecdcecc1
Muhammad Bisyri Mustofa <mbisyri33@gmail.com>
2024-06-27 09:33:33 +0700

    CRUD permission & update drop down menu

--- server/addons/estate/__manifest__.py
+++ server/addons/estate/__manifest__.py
@@ -9,4 +9,8 @@
                         This is module is used to learn basic technical Odoo 17
                     """,
     'application': True,
+    'data': [
+        'security/ir.model.access.csv',    
+        'views/menu.xml',
+    ]
 }
