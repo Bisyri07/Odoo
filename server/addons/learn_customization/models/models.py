ecbe4cd232e0097f51ff1db8f97262af2bae8fe3
Muhammad Bisyri Mustofa <mbisyri33@gmail.com>
2024-08-08 10:22:13 +0700

    odoo scaffold

---
+++ server/addons/learn_customization/models/models.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class learn_customization(models.Model):
+#     _name = 'learn_customization.learn_customization'
+#     _description = 'learn_customization.learn_customization'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+
+# class EstatePropertyTagInheritance(models.Model):
+#     _inherit = "estate.property.tag"
+
+#     priority = fields.Integer(string="Tag Priority", default=0 )
+    # color = fields.Integer(string="Color Index Overriden field", required=True)
+
+    # def inherited_action(self):
+    #     return super().inherited_action()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
